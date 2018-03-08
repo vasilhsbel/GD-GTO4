38732f1534a31d49cfe5c4bad98d9b149253c458
Vasilis <vasilhsbel@hotmail.com>
2018-03-08 20:30:14 +0100

    First Commit
    Resources Example

---
+++ GTO4_Project/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour {
+
+    public Resource myResource;
+
+    public void increaseButton()
+    {
+        myResource.increaseAmmount(10);
+    }
+
+    public void decreaseButton()
+    {
+        myResource.reduceAmmount(10);
+    }
+}
