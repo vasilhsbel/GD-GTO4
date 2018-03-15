bcdc9728ae048ae7d5710dd6ae0f70e444d04dc7
Vasilis <vasilhsbel@hotmail.com>
2018-03-15 09:10:17 +0100

    Spawning
    Spawn units on empty tiles

---
+++ GTO4_Project/Assets/Scripts/Grid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public Tile tilePrefab;
+    public Vector2 mapSize;
+
+    //[Range(0,1)]
+    //public float outlinePercent;
+
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+    
+    
+    public Tile getEmptyTile()
+    {
+        Tile[] allTiles = GetComponentsInChildren<Tile>();
+        foreach (Tile tile in allTiles)
+        {               
+            if (tile.emptyTile)
+                return tile;
+        }
+        return null;
+    }
+    
+    public void GenerateMap()
+    {
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y/2 + 0.5f + y);
+                Tile newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right));
+                newTile.transform.parent = gameObject.transform;
+                //newTile.localScale =new Vector3(1,1,1/10) * (1-outlinePercent);
+            }
+        }
+    }
+}
---
+++ GTO4_Project/Assets/Scripts/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public bool emptyTile;
+
+    public void Start()
+    {
+        emptyTile = true;
+    }
+
+    public void OnTriggerStay()
+    {
+        this.emptyTile = false;
+    }
+
+    
+    public void OnTriggerExit()
+    {
+       this.emptyTile = true;
+    }   
+    
+
+}
